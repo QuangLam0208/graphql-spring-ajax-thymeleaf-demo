486045a432de51600daf046f0347810c85d1a30f
Quang Lam <laulua1223@outlook.com>
2025-12-12 08:13:13 +0700

    first commit

---
+++ src/main/java/ltweb/GraphqlSpringAjaxThymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package ltweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringAjaxThymeleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringAjaxThymeleafDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ltweb/GraphqlSpringAjaxThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ltweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringAjaxThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
